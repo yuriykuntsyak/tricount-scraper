e6ab832d820e94201d7b955a958321e505e0574f
yuriykuntsyak <yuriykuntsyak@gmail.com>
2021-10-31 16:46:51 +0000

    Add skip submission check

--- const.py
+++ const.py
@@ -1,14 +1,6 @@
-from logging import INFO
-from os import getenv
-
 from selenium.webdriver.common.by import By
 
-TRICOUNT_URL = getenv("TRICOUNT_URL", default="")
-USER_NAME = getenv("USER_NAME", default="")
-LOGLEVEL = getenv("LOGLEVEL", default=INFO)
-EXPENSE_DESCRIPTION = getenv("EXPENSE_DESCRIPTION", default="")
-EXPENSE_AMOUNT = getenv("EXPENSE_AMOUNT", default="")
-EXPENSE_DATE = getenv("EXPENSE_DATE", default="")
+DRIVER_WAIT_TIMEOUT = 3
 
 PAID_FOR_USERS = '//div[@class="repartitionNameLabel"]'
 SAVE_PAYMENT_XPATH = (
@@ -21,7 +13,6 @@ PAYMENT_DESCRIPTION_XPATH = '//*[@id="slot1"]/table/tbody/tr[4]/td/table/tbody/t
 ADD_EXPENSE_XPATH = (
     '//*[@id="slot1"]/table/tbody/tr[5]/td/div/table/tbody/tr/td/table/tbody/tr/td/a'
 )
-USER_XPATH = f'//*[@id="slot1"]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/div/table/tbody/tr/td[1]/div/div/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td[2]/div/div/div[text()="{USER_NAME}"]'  # noqa
 IFRAME_ID = "module-web"
 PAYMENTS_TABLE_XPATH = (
     '//*[@id="slot1"]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/div/div/div/table'
@@ -30,7 +21,25 @@ PAYMENTS_TABLE_CLASS = "paymentPanel"
 EXPENSES_DIVS_XPATH = (
     '//div[@class="paymentListContent"]|//a[@class="paymentListContent"]'  # mix of div and a
 )
-DRIVER_WAIT_TIMEOUT = 3
+
+USERS_LIST_XPATH = (
+    '//*[@id="slot1"]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/div/'
+    "table/tbody/tr/td[1]/div/div/table/tbody/tr[1]/td/"
+    "table/tbody/tr/td/table/tbody/tr/td[2]/div/div/div"
+)
+
+ELEMENTS_PER_PAGE = {
+    "pre_users_list": {"locator": By.ID, "elements": [IFRAME_ID]},
+    "users_list": {"locator": By.XPATH, "elements": []},  # TBD, for now the val is assigned in main
+    "expenses_list": {
+        "locator": By.XPATH,
+        "elements": [ADD_EXPENSE_XPATH, PAYMENTS_TABLE_XPATH],
+    },
+    "expense_form": {
+        "locator": By.XPATH,
+        "elements": [SAVE_PAYMENT_XPATH, PAYMENT_AMOUNT_XPATH],
+    },
+}
 
 PAGE_NAV_ORDER = [
     "pre_users_list",
@@ -39,9 +48,3 @@ PAGE_NAV_ORDER = [
     "expense_form",
     "invalid_url",
 ]
-ELEMENTS_PER_PAGE = {
-    "pre_users_list": {"locator": By.ID, "elements": [IFRAME_ID]},
-    "users_list": {"locator": By.XPATH, "elements": [USER_XPATH]},
-    "expenses_list": {"locator": By.XPATH, "elements": [ADD_EXPENSE_XPATH, PAYMENTS_TABLE_XPATH]},
-    "expense_form": {"locator": By.XPATH, "elements": [SAVE_PAYMENT_XPATH, PAYMENT_AMOUNT_XPATH]},
-}
